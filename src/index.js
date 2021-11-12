2bbc149a0b2b5572b5dc5d684ffd11ce2ec12af1
arike-liasu <aishatliasu17965@gmail.com>
2021-11-12 10:34:10 +0100

    feat: add routes for all the pages

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import './index.css';
+import App from './components/App';
+import middleware from './middleware';
+import reducer from './reducers';
+
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
